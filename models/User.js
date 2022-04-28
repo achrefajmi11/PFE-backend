d31b14d30d9ac73a6bdba6f1df832e79847678fe
iskanderAB <iskanderabasi@gmail.com>
2022-04-28 01:05:05 +0100

    fix server connection

--- models/User.js
+++ models/User.js
@@ -37,7 +37,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "user",
     }
   );
   return User;
--- server.js
+++ server.js
@@ -5,9 +5,3 @@ app.use(express.urlencoded({extended: true}));
 const controller = require("./controller/controller");
 controller.init()
 app.use(express.json());
-
-
-
-db.sequelize.sync().then(() => {
-    app.listen(4100,()=> console.log("serveur listening in port 3600"))
-})
